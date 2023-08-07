4d570c915937fa4f48e5bf4a79106e1d9125287c
prabinlamsal19 <lamsalprabin74@gmail.com>
2023-08-07 16:41:36 +0545

    feat: adding books to db

--- books/books.js
+++ books/books.js
@@ -1,13 +1,40 @@
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
+const { error } = require('console');
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+require("./Book");
 
+
+const Book = mongoose.model("Book");
 mongoose.connect("mongodb+srv://prabinlamsal:mKVIj2ehxVoKQqhB@cluster0.92rvbxj.mongodb.net/?retryWrites=true&w=majority");
+
+app.use(express.json(
+
+));
 
 app.get('/', (req, res) => {
     res.send("Main endpoint from books");
 })
 
+app.post("/book", (req, res) => {
+    console.log(req.body);
+
+    var newBook = {
+        title: req.body.title,
+        author: req.body.author,
+        numberPages: req.body.numberPages,
+        publisher: req.body.publisher
+    };
+    var book = new Book(newBook);
+
+    book.save().then(() => {
+        res.send("New book created")
+    }).catch((error) => {
+        res.send(error);
+    })
+})
+
+
 app.listen(4545, () => {
     console.log("Book Service up and running at port 4545");
-})
+}) 
